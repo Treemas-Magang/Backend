0a605998d185d078a4417bd5a8adc79909bb6eb6
kenthona <86694449+kenthona@users.noreply.github.com>
2023-11-02 10:02:16 +0700

    udah dibenerin data type dari tiap repo

--- src/main/java/com/treemaswebapi/treemaswebapi/repository/SysUserBranchRepository.java
+++ src/main/java/com/treemaswebapi/treemaswebapi/repository/SysUserBranchRepository.java
@@ -4,8 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.treemaswebapi.treemaswebapi.entity.SysUserEntity.SysUserBranchEntity;
-import com.treemaswebapi.treemaswebapi.entity.SysUserEntity.SysUserBranchKey;
 
 @Repository
-public interface SysUserBranchRepository extends JpaRepository<SysUserBranchEntity, SysUserBranchKey> {
+public interface SysUserBranchRepository extends JpaRepository<SysUserBranchEntity, String> {
 }

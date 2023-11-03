eb23a3ca5439d5e6cf17d671a0cf2831c898e10c
divannisaa <divannisae@gmail.com>
2023-11-03 10:17:56 +0700

    cors

--- src/main/java/com/treemaswebapi/treemaswebapi/config/SecurityConfiguration.java
+++ src/main/java/com/treemaswebapi/treemaswebapi/config/SecurityConfiguration.java
@@ -1,5 +1,7 @@
 package com.treemaswebapi.treemaswebapi.config;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
@@ -9,6 +11,9 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
@@ -23,10 +28,9 @@ public class SecurityConfiguration {
 
     @Bean
      public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
+        http     
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/master-data/karyawan-form/add").permitAll()
                         .requestMatchers("/api/auth/login").permitAll()
                         .anyRequest().authenticated()
                 )
@@ -39,7 +43,21 @@ public class SecurityConfiguration {
                     response.getWriter().write("Invalid Token");
                 })
             )
-                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
+                .cors(cors -> cors.configurationSource(corsConfigurationSource())); // Konfigurasi CORS disini
         return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration corsConfig = new CorsConfiguration();
+        corsConfig.setAllowedOrigins(List.of("*")); // Izinkan akses dari semua origin
+        corsConfig.addAllowedHeader("*");
+        corsConfig.addAllowedMethod("*");
+        
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfig);
+        
+        return source;
     }
 }

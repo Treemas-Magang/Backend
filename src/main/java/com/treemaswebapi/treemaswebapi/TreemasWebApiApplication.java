95c09c940d0eea8d61040a7e3e71f1e72efde3fc
kenthona <86694449+kenthona@users.noreply.github.com>
2023-12-14 11:35:05 +0700

    nambahin EnableScheduling di main class, sama nambahin SchedulerService yang diset ke jam 12siang atau 05.00UTC

--- src/main/java/com/treemaswebapi/treemaswebapi/TreemasWebApiApplication.java
+++ src/main/java/com/treemaswebapi/treemaswebapi/TreemasWebApiApplication.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 @EntityScan(basePackages = "com.treemaswebapi.treemaswebapi.entity")
+@EnableScheduling
 public class TreemasWebApiApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/treemaswebapi/treemaswebapi/service/SchedulerService/SchedulerService.java
@@ -0,0 +1,35 @@
+package com.treemaswebapi.treemaswebapi.service.SchedulerService;
+
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.util.List;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import com.treemaswebapi.treemaswebapi.entity.AbsenEntity.AbsenEntity;
+import com.treemaswebapi.treemaswebapi.entity.KaryawanEntity.KaryawanEntity;
+import com.treemaswebapi.treemaswebapi.repository.AbsenRepository;
+import com.treemaswebapi.treemaswebapi.repository.KaryawanRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class SchedulerService {
+    private final KaryawanRepository karyawanRepository;
+    private final AbsenRepository absenRepository;
+
+    @Scheduled(cron = "0 0 5 * * ?")
+    public void createAbsenEntries() {
+        List<KaryawanEntity> dataKaryawan = karyawanRepository.findAll();
+        for (KaryawanEntity karyawan : dataKaryawan) {
+            AbsenEntity absenEntity = new AbsenEntity();
+            absenEntity.setNik(karyawan.getNik());
+            absenEntity.setDtmCrt(Timestamp.valueOf(LocalDateTime.now()));
+            absenRepository.save(absenEntity);
+            System.out.println("scheduled job has been executed");
+        }
+    }
+}
+

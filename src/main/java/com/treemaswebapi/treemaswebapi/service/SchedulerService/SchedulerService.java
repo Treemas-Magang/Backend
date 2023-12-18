b6e59408b40723d82d01dcf40a7dd185570091b8
kenthona <86694449+kenthona@users.noreply.github.com>
2023-12-18 12:54:42 +0700

    ganti scheduler ke jam 1

--- src/main/java/com/treemaswebapi/treemaswebapi/service/SchedulerService/SchedulerService.java
+++ src/main/java/com/treemaswebapi/treemaswebapi/service/SchedulerService/SchedulerService.java
@@ -21,7 +21,7 @@ public class SchedulerService {
     private final KaryawanRepository karyawanRepository;
     private final AbsenRepository absenRepository;
 
-    @Scheduled(cron = "0 0 0 * * ?")
+    @Scheduled(cron = "0 0 13 * * ?")
     public void createAbsenEntries() {
         List<KaryawanEntity> dataKaryawan = karyawanRepository.findAll();
         for (KaryawanEntity karyawan : dataKaryawan) {
